343e5e6dc6029541f537df2ec416446413dfa50f
Tony Bowden <tony@tmtm.com>
2017-02-05 16:01:36 +0000

    Extract MemberPage class

--- scraper.rb
+++ scraper.rb
@@ -26,10 +26,10 @@ end
 
 class LinkLine < Scraped::HTML
   field :name do
-    noko.text
+    noko.text.tidy
   end
 
-  field :url do
+  field :source do
     noko.attr('href')
   end
 end
@@ -42,43 +42,39 @@ class GroupPage < SobraniePage
   end
 end
 
+class MemberPage < SobraniePage
+  field :id do
+    url.to_s[/ns_article-(.*?)-(\d+)/, 1]
+  end
+
+  field :image do
+    images.size.zero? ? '' : images.first.text
+  end
+
+  private
+
+  def box
+    noko.css('.article-holder')
+  end
+
+  def images
+    box.css('img/@src')
+  end
+end
+
 def scrape(h)
   url, klass = h.to_a.first
   klass.new(response: Scraped::Request.new(url: url).response)
 end
 
-def noko_for(url)
-  Nokogiri::HTML(open(url).read)
-end
-
-def scrape_list(url)
-  scrape(url => GroupsPage).groups.each do |group|
-    scrape_group(group.name, group.url)
-  end
-end
-
-def scrape_group(name, url)
-  scrape(url => GroupPage).members.each do |mem|
-    scrape_person(mem.url, mem.name, name)
-  end
-end
-
-def scrape_person(url, name, group)
-  noko = noko_for(url)
-
-  box = noko.css('.article-holder')
-  images = box.css('img/@src')
-  data = {
-    id:     url.to_s[/ns_article-(.*?)-(\d+)/, 1],
-    name:   name.tidy,
-    party:  group.tidy,
-    image:  images.size.zero? ? '' : images.first.text,
-    term:   2014,
-    source: url.to_s,
-  }
-  data[:image] = URI.join(url, URI.escape(data[:image])).to_s unless data[:image].to_s.empty?
-  ScraperWiki.save_sqlite(%i(id term), data)
-end
-
 ScraperWiki.sqliteexecute('DELETE FROM data') rescue nil
-scrape_list('http://sobranie.mk/current-structure-2014-2018.nspx')
+
+start = 'http://sobranie.mk/current-structure-2014-2018.nspx'
+data = scrape(start => GroupsPage).groups.flat_map do |group|
+  scrape(group.source => GroupPage).members.map do |mem|
+    mem.to_h.merge(scrape(mem.source => MemberPage).to_h.merge(party: group.name, term: 2014))
+  end
+end
+# puts data.map { |r| r.reject { |k, v| v.to_s.empty? }.sort_by { |k,v| k }.to_h }
+
+ScraperWiki.save_sqlite(%i(id term), data)
