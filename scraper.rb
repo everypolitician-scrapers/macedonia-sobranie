aebed5369dd687ec5384528559cef2ac3aaf0d7b
Oliver Denman <oliver@therewillbeanimals.com>
2017-02-10 14:13:46 +0000

    Scrape ceased members

--- scraper.rb
+++ scraper.rb
@@ -21,11 +21,26 @@ end
 ScraperWiki.sqliteexecute('DELETE FROM data') rescue nil
 
 start = 'http://sobranie.mk/current-structure-2014-2018.nspx'
-data = scrape(start => GroupsPage).groups.flat_map do |group|
+groups = scrape(start => GroupsPage).groups
+# The structure of the last page differs from the others.
+# We will scrape it separately.
+party_groups = groups[0...-1]
+ceased_members_group = groups.last
+term = 2014
+
+current_members = party_groups.flat_map do |group|
+  puts group.source
   scrape(group.source => GroupPage).members.map do |mem|
-    mem.to_h.merge(scrape(mem.source => MemberPage).to_h.merge(party: group.name, term: 2014))
+    mem.to_h.merge(scrape(mem.source => MemberPage).to_h.merge(party: group.name, term: term))
   end
 end
+
+ceased_members = scrape(ceased_members_group.source => CeasedMembersPage).members.map do |mem|
+  mem.to_h.merge(scrape(mem.source => MemberPage).to_h.merge(party: mem.party, term: term))
+end
+
+data = (current_members << ceased_members).flatten
+
 # puts data.map { |r| r.reject { |k, v| v.to_s.empty? }.sort_by { |k,v| k }.to_h }
 
 ScraperWiki.save_sqlite(%i(id term), data)
